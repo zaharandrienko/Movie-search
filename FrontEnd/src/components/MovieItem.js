46a4876b1c6a474ec280af52c7e39311aa923f4b
Zahar Andrienko <zaharandrienko88@gmail.com>
2019-03-05 23:00:44 +0200

    fix grid

--- FrontEnd/src/components/MovieItem.js
+++ FrontEnd/src/components/MovieItem.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class MovieItem extends Component {
     render() {
         let mov = this.props.movie;
-      
+
         let photo = {
             backgroundImage: "url(" + mov.Poster + ")"
         }
